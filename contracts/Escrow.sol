53ce151a363bb29f0d6b64067d33a97739723154
pfed-prog <fedorovn19@gmail.com>
2023-07-18 16:28:51 +0200

    compiles

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,21 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity =0.8.9;
+pragma solidity =0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 // connect to Dispenser, Factory
 
-contract Escrow {
-    address payable public admin;
+contract Escrow is ReentrancyGuard {
+    address public admin;
+    uint internal locked;
 
-    constructor(address _router) payable {
+    event MultiSended(uint _total, address _token);
+
+    constructor(address _router) {
         admin = _router;
     }
 
-function multisendToken(address token, address[] _contributors, uint256[] _balances) public hasFee payable {
+function multisendToken(address token, address[] calldata _contributors, uint256[] calldata _balances) public payable {
     require(msg.sender == admin, "You aren't the owner");
     uint total;
-    uint lengthContributors = _contributors.length
+    uint lengthContributors = _contributors.length;
     // change to template
     ERC20 erc20token = ERC20(token);
     for (uint i; i!=lengthContributors; ) {
@@ -23,7 +27,7 @@ function multisendToken(address token, address[] _contributors, uint256[] _balan
         total += _balances[i];
         unchecked{++i;}
     }
-    MultiSended(total, token);
+    emit MultiSended(total, token);
     }
 
 }
