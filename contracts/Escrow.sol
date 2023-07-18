4b20f5647f39cb2fdfddae991b67eadb4c5be619
pfed-prog <fedorovn19@gmail.com>
2023-07-18 15:46:09 +0200

    reduced code

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity =0.8.9;
 
-// connect to Dispenser
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-// Connect to Factory
+// connect to Dispenser, Factory
 
 contract Escrow {
     address payable public admin;
@@ -12,27 +12,18 @@ contract Escrow {
         admin = _router;
     }
 
-    function distribute(address to, uint amount) public {
-        require(msg.sender == admin, "You aren't the owner");
-
-        // add batch transfer
-
-        //emit Distribution(to, amount, block.timestamp);
-    }
-
 function multisendToken(address token, address[] _contributors, uint256[] _balances) public hasFee payable {
     require(msg.sender == admin, "You aren't the owner");
-
+    uint total;
     uint lengthContributors = _contributors.length
-    require(lengthContributors <= arrayLimit());
-    uint256 total;
+    // change to template
     ERC20 erc20token = ERC20(token);
-    for (uint i; i <  lengthContributors; i++) {
+    for (uint i; i!=lengthContributors; ) {
         erc20token.transferFrom(msg.sender, _contributors[i], _balances[i]);
         total += _balances[i];
+        unchecked{++i;}
     }
-    // setTxCount(msg.sender, txCount(msg.sender).add(1));
-    Multisended(total, token);
-}
+    MultiSended(total, token);
+    }
 
 }
